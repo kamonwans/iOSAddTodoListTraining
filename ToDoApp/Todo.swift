74251389489910306bf9f1b5382df44e2dbc7670
kamonwans <kamonwan294@gmail.com>
2018-07-22 11:40:47 +0700

    bindding data to tableview

--- ToDoApp/Todo.swift
+++ ToDoApp/Todo.swift
@@ -48,7 +48,7 @@ class Todo{
 class TodoItem{
     var title: String
     var isDone:Bool
-    init(title:String,isDone:Bool) {
+    init(title:String,isDone:Bool=false) {
         self.title = title
         self.isDone = isDone
     }
--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -8,16 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource {
+    var todo = Todo()
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todo.totalItems
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
+        cell.textLabel?.text = todo.item(at: indexPath.row).title
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        todo.add(item: TodoItem(title: "Test"))
+        todo.add(item: TodoItem(title: "Learning Switf 3"))
+        
     }
 
 

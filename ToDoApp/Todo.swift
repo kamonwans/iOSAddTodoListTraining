92f3dbb2f9a72fd988e501ecf88ef18449ba9de1
kamonwans <kamonwan294@gmail.com>
2018-07-22 11:20:54 +0700

    add table view to storyboard and create model todo and todo item

---
+++ ToDoApp/Todo.swift
@@ -0,0 +1,60 @@
+//
+//  Todo.swift
+//  ToDoApp
+//
+//  Created by liuyao on 22/7/2561 BE.
+//  Copyright © 2561 liuyao. All rights reserved.
+//
+
+import Foundation
+
+class Todo{
+    private var items = [TodoItem]()
+    
+    var totalItems: Int {
+        return items.count
+    }
+    
+    func add(item: TodoItem){
+        items.insert(item,at:0)
+    }
+    
+    func remove(at index:Int){
+        items.remove(at: index)
+    }
+    
+    func item(at index: Int) -> TodoItem{
+        return items[index]
+    }
+    
+    func index(of item: TodoItem) -> Int?{
+        return items.index(where: { (todoItem)  in
+             todoItem === item
+        })
+        
+        
+//        return items.index{ (todoItem)  in
+//            todoItem === item
+//        }
+        
+//        return items.index{
+//            $0 === item
+//        }
+        
+        
+    }
+}
+
+class TodoItem{
+    var title: String
+    var isDone:Bool
+    init(title:String,isDone:Bool) {
+        self.title = title
+        self.isDone = isDone
+    }
+    
+    func toggleIsDone(){
+        isDone = !isDone
+    }
+}
+
